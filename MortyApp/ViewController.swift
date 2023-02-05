7c962f52730d060aeb181643b2b866261d738236
KhaledHamdySalem <khaled.hamdy2290@gmail.com>
2023-02-05 20:20:18 +0300

    Initial Commit

---
+++ MortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortyApp
+//
+//  Created by KH on 05/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
